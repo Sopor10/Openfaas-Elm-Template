21746e091289d65dc608edd8395da7ca0620cf90
Lars <Lars@DESKTOP-1788R46>
2021-01-02 15:14:13 +0100

    initial elm Template

---
+++ function/src/Handler.elm
@@ -0,0 +1,32 @@
+module Handler exposing (..)
+
+import Json.Decode as D
+import Json.Encode as E
+
+
+type alias Input =
+    List Int
+
+
+type alias Output =
+    Int
+
+
+run : Input -> Result String Output
+run input =
+    case input of
+        [] ->
+            Err "to short"
+
+        _ ->
+            Ok <| List.length input
+
+
+decoder : D.Decoder Input
+decoder =
+    D.list D.int
+
+
+encoder : Output -> E.Value
+encoder =
+    E.int
---
+++ handler.js
@@ -0,0 +1,58 @@
+'use strict'
+const { randomBytes } = require("crypto");
+const { Elm } = require("./function/elm-main");
+module.exports = async (event, context) => {
+  const result = {
+    
+    'status':  await run("f1",JSON.stringify(event.body))
+  }
+
+  return context
+    .status(200)
+    .succeed(result)
+}
+
+
+const App = Elm.Main.init({});
+
+const run = async (functionId, input) => {
+  const output = App.ports.output;
+  const start = App.ports.start;
+
+  const jobId = randomBytes(16).toString("hex");
+  const p = new Promise(resolve => {
+    try {
+      let timeout;
+      const go = v => {
+        if (v.jobId === jobId) {
+          clearTimeout(timeout);
+          output.unsubscribe(go);
+
+          if (v.status === "ok") {
+            resolve({ ok: v.output });
+          } else if (v.status === "error") {
+            resolve({ error: v.msg });
+          } else {
+            resolve({ error: "invalid response status" });
+          }
+        }
+      };
+
+      output.subscribe(go);
+
+      timeout = setTimeout(() => {
+        output.unsubscribe(go);
+        resolve({ error: "invalid jobId or time limit exceeded" });
+      }, 20 * 1000);
+    } catch (e) {
+      resolve({ error: "unexpected error" });
+    }
+  });
+
+  setTimeout(() => {
+    start.send({ jobId, functionId, input });
+  }, Math.random() * 1000);
+
+  return p;
+};
+

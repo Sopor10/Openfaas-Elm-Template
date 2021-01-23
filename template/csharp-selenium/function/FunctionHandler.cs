6fb7679e177e33226119441772f21e6905044f6a
Lars <larslehmann@kabelmail.de>
2021-01-23 18:02:36 +0100

    copy gecko driver

--- template/csharp-selenium/function/FunctionHandler.cs
+++ template/csharp-selenium/function/FunctionHandler.cs
@@ -1,6 +1,9 @@
 using Microsoft.AspNetCore.Http;
 using System.IO;
 using System.Threading.Tasks;
+using System.Text.Json;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
 
 namespace Function
 {
@@ -9,9 +12,20 @@ namespace Function
         public async Task<(int, string)> Handle(HttpRequest request)
         {
             var reader = new StreamReader(request.Body);
-            var input = await reader.ReadToEndAsync();
+            var input = JsonSerializer.Deserialize<Input>(await reader.ReadToEndAsync());
 
-            return (200, $"Hello! Your input was {input}");
+
+            var firefoxOptions = new FirefoxOptions();
+            firefoxOptions.AddArgument("-headless");
+            using IWebDriver driver = new FirefoxDriver(System.Environment.CurrentDirectory,firefoxOptions);
+
+            driver.Navigate().GoToUrl(input.Url);
+            
+            return (200, driver.PageSource);
         }
+    }
+    public record Input
+    {
+        public string Url { get; init; }
     }
 }
